451973b77fb6c01caf073e5643528b4b4be603ba
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-23 18:13:59 +0000

    Add AV Cat C cross-encoder: dataset class

--- scripts/train_av_cat_c_crossenc.py
+++ scripts/train_av_cat_c_crossenc.py
@@ -16,3 +16,28 @@ sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.data_utils import load_av_data, load_solution_labels, save_predictions
 from src.scorer import compute_all_metrics, print_metrics
+
+
+class AVCrossEncoderDataset(Dataset):
+    """AV dataset for cross-encoder: tokenize text_1 + text_2 as pair."""
+    def __init__(self, df, tokenizer, max_len=384):
+        self.texts_1 = list(df['text_1'])
+        self.texts_2 = list(df['text_2'])
+        self.labels = df['label'].values.astype(np.float32)
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        enc = self.tokenizer(
+            self.texts_1[idx], self.texts_2[idx],
+            truncation=True, max_length=self.max_len,
+            padding='max_length', return_tensors='pt'
+        )
+        return {
+            'input_ids': enc['input_ids'].squeeze(0),
+            'attention_mask': enc['attention_mask'].squeeze(0),
+            'label': torch.tensor(self.labels[idx], dtype=torch.float),
+        }
