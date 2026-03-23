7707ebf96da2baa7c59905d54fbed4b738020add
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-23 01:41:03 +0000

    Add NLI Cat C demo: model loading

--- notebooks/demo_nli_cat_c.py
+++ notebooks/demo_nli_cat_c.py
@@ -27,3 +27,14 @@ from transformers import AutoTokenizer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: {device}")
+
+# %% [markdown]
+# ## 1. Load Trained Model
+
+# %%
+MODEL_NAME = 'microsoft/deberta-v3-base'
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=False)
+
+model = NLIDeBERTaCrossEncoder(model_name=MODEL_NAME).to(device)
+model.load_state_dict(torch.load('models/nli_cat_c_best.pt',
+                                  map_location=device, weights_only=True))
