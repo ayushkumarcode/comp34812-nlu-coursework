ab780f41fabc08f25d002ed89e197b57b2fb2829
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-23 15:39:32 +0000

    Add NLI Cat A RF: data loading and feature extraction

--- scripts/iter_nli_a_rf.py
+++ scripts/iter_nli_a_rf.py
@@ -9,3 +9,15 @@ from sklearn.ensemble import RandomForestClassifier
 from src.data_utils import load_nli_data, load_solution_labels, save_predictions
 from src.nli_pipeline import NLIFeatureExtractor
 from src.scorer import compute_all_metrics, print_metrics
+
+train_df = load_nli_data(split='train')
+dev_df = load_nli_data(split='dev')
+y_train = train_df['label'].values
+y_dev = np.array(load_solution_labels(task='nli'))
+ext = NLIFeatureExtractor(use_spacy=True, use_glove=False)
+ext.fit(train_df)
+X_train, _ = ext.transform(train_df)
+X_dev, _ = ext.transform(dev_df)
+scaler = StandardScaler()
+X_tr = scaler.fit_transform(X_train)
+X_dv = scaler.transform(X_dev)
