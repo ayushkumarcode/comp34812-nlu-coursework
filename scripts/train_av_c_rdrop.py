bece3b686b2e5f14a48595d65ba5a17f6ad118f6
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-24 23:01:35 +0000

    Add AV Cat C R-Drop training: lines 29-56

--- scripts/train_av_c_rdrop.py
+++ scripts/train_av_c_rdrop.py
@@ -26,3 +26,31 @@ from src.scorer import compute_all_metrics, print_metrics
 
 
 class AVCrossEncoderDataset(Dataset):
+    """AV dataset for cross-encoder: tokenize text_1 + text_2 as pair."""
+    def __init__(self, df, tokenizer, max_len=384):
+        self.texts_1 = list(df['text_1'])
+        self.texts_2 = list(df['text_2'])
+        self.labels = df['label'].values.astype(np.float32)
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        enc = self.tokenizer(
+            self.texts_1[idx], self.texts_2[idx],
+            truncation=True, max_length=self.max_len,
+            padding='max_length', return_tensors='pt'
+        )
+        return {
+            'input_ids': enc['input_ids'].squeeze(0),
+            'attention_mask': enc['attention_mask'].squeeze(0),
+            'label': torch.tensor(self.labels[idx], dtype=torch.float),
+        }
+
+
+class AVCrossEncoder(nn.Module):
+    """Simple cross-encoder: DeBERTa + classification head."""
+    def __init__(self, model_name='microsoft/deberta-v3-base'):
+        super().__init__()
