2306e15ae249eac11859ba6aaf1d56e2ae4e1683
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-24 18:24:30 +0000

    Add AVCrossEncoderDatasetV2 with topic labels

--- scripts/train_av_cat_c_crossenc_v2.py
+++ scripts/train_av_cat_c_crossenc_v2.py
@@ -24,3 +24,32 @@ from src.data_utils import load_av_data, load_solution_labels, save_predictions
 from src.scorer import compute_all_metrics, print_metrics
 from src.models.cat_c_deberta import ScalarMix, GRL
 from src.models.av_cat_b_dataset import generate_topic_labels
+
+
+class AVCrossEncoderDatasetV2(Dataset):
+    """AV dataset for cross-encoder with topic pseudo-labels."""
+    def __init__(self, df, tokenizer, max_len=384, topic_labels=None):
+        self.texts_1 = list(df['text_1'])
+        self.texts_2 = list(df['text_2'])
+        self.labels = df['label'].values.astype(np.float32)
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.topic_labels = topic_labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        enc = self.tokenizer(
+            self.texts_1[idx], self.texts_2[idx],
+            truncation=True, max_length=self.max_len,
+            padding='max_length', return_tensors='pt'
+        )
+        item = {
+            'input_ids': enc['input_ids'].squeeze(0),
+            'attention_mask': enc['attention_mask'].squeeze(0),
+            'label': torch.tensor(self.labels[idx], dtype=torch.float),
+        }
+        if self.topic_labels is not None:
+            item['topic'] = torch.tensor(self.topic_labels[idx], dtype=torch.long)
+        return item
